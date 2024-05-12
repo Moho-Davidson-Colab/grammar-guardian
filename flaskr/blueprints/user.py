fe6298938145b5b496a6651246bb8a8101dfd042
vanhugee <hunguyen@davidson.edu>
2024-05-12 02:42:02 -0400

    Test post to db

--- flaskr/blueprints/user.py
+++ flaskr/blueprints/user.py
@@ -1,10 +1,17 @@
 from flaskr.models.user import User
 from flask import Blueprint, request
+from flaskr.test_mongo import get_collection
 
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/user', methods=['POST'])
 def create_user():
-    user_info = request.get_json()
-    user_object = User(**user_info)
-    return user_object.model_dump()
+    try:
+        user_info = request.get_json()
+        user_object = User(**user_info)
+        collection = get_collection()
+        result = collection.insert_one(user_object.model_dump())
+        print(result.acknowledged)
+        return user_object.model_dump()
+    except Exception as e:
+        return str(e), 400
--- flaskr/test_mongo.py
+++ flaskr/test_mongo.py
@@ -4,15 +4,15 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 
-
-load_dotenv()
-uri = os.getenv('MONGO_URI')
-
-try:
-    client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-    db = client.get_database('sample_mflix')
-    test_collection = db["test_collection"]
-    print(db.list_collection_names())
-except Exception as e:
-    print(e)
+def get_collection():
+    load_dotenv()
+    uri = os.getenv('MONGO_URI')
+    try:
+        client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        db = client.get_database('sample_mflix')
+        collection = db["test"]
+        return collection
+    except Exception as e:
+        print(e)
+        return collection
