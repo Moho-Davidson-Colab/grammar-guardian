af6829372303ce0b70ad8acb0b97d89a2285273d
vanhugee <hunguyen@davidson.edu>
2024-06-02 23:57:19 -0400

    Add mui to signin/signup page

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,12 +1,25 @@
-import { Sign } from "crypto";
+import React, { useState } from "react";
 import { SignUp } from "../components/signUp";
 import { SignIn } from "../components/signIn";
+import Test from "../components/login";
+import Register from "../components/register";
+import { Button, Container, Box } from "@mui/material";
 
 export function Auth() {
+  const [showSignIn, setShowSignIn] = useState(true);
+
+  const toggleView = () => {
+    setShowSignIn(!showSignIn);
+  };
+
   return (
-    <div>
-      <SignUp />
-      <SignIn />
-    </div>
+    <Container>
+      <Box display="flex" justifyContent="center" mt={2}>
+        <Button variant="contained" onClick={toggleView}>
+          {showSignIn ? "Switch to Sign Up" : "Switch to Sign In"}
+        </Button>
+      </Box>
+      {showSignIn ? <Test /> : <Register />}
+    </Container>
   );
 }
